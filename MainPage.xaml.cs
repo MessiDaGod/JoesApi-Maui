506497cbed6f7399e0a6a2a708a59baa02cecaef
joeshakely <joeshakely@gmail.com>
2022-09-12 12:31:56 -0700

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace JoesApi_Maui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ run.sh
@@ -0,0 +1,2 @@
+!#/bin/sh
+dotnet build -t:Run -f net6.0-ios
